2486b2f9736f923f126a79dc0ef4f867e084fe2a
Wico <wicobuys@gmail.com>
2024-07-07 16:22:26 +0200

    initial commit

--- web.py
+++ web.py
@@ -20,7 +20,7 @@ for index, todo in enumerate(todos):
         todos.pop(index)
         functions.write_todos(todos)
         del st.session_state[todo]
-        st.experimental_rerun()
+        st.rerun()
 
 st.text_input(label="Enter a todo", label_visibility='hidden',
               placeholder="Add new todo...",
